516d9237a797ffaf447a3ab19652c5714efd5f41
Kostess <qaz1234567890838@gmail.com>
2023-11-05 05:35:38 +0300

    Доработка

--- learnOOP/learnOOP.cpp
+++ learnOOP/learnOOP.cpp
@@ -1,4 +1,5 @@
 ﻿#include <iostream>
+#include <iomanip>
 
 using namespace std;
 
@@ -54,7 +55,11 @@ protected:
     double _z{ 0.0 };
 
 public:
-    Point3D(double x, double y, double z) : Point2D(x, y), _z(z) {};
+    Point3D(double x, double y) : Point2D(x, y) {};
+
+    Point3D(double x, double y, double z) : Point3D(x, y)  {
+        _z = z;
+    };
 
     ~Point3D() {
         _z = 0.0;
@@ -72,12 +77,15 @@ public:
 
 class Section : public Figure {
 private:
-    Point2D* startPoint;
-    Point2D* endPoint;
+    Point3D* startPoint;
+    Point3D* endPoint;
 
 public:
-    Section(Point2D* startPoint, Point2D* endPoint) : startPoint(startPoint), endPoint(endPoint) {}
+    Section(Point3D* startPoint, Point3D* endPoint) : startPoint(startPoint), endPoint(endPoint) {}
 
+    ~Section() {
+
+    }
 
 public:
     void print() {
@@ -96,83 +104,57 @@ public:
 
 class Triangle : public FlatFigure {
 protected:
-    Point2D* point2D_1;
-    Point2D* point2D_2;
-    Point2D* point2D_3;
-
-    Point3D* point3D_1;
-    Point3D* point3D_2;
-    Point3D* point3D_3;
+    Point3D* point1;
+    Point3D* point2;
+    Point3D* point3;
 
 public:
-    Triangle(Point2D* point1, Point2D* point2, Point2D* point3) : point2D_1(point1), point2D_2(point2), point2D_3(point3) {}
-    Triangle(Point3D* point1, Point3D* point2, Point3D* point3) : point3D_1(point1), point3D_2(point2), point3D_3(point3) {}
+    Triangle(Point3D* point1, Point3D* point2, Point3D* point3) : point1(point1), point2(point2), point3(point3) {}
+
+    ~Triangle() {
+
+    }
 
 public:
     void print() {
         cout << "Triangle ";
-        point2D_1->print();
-        point2D_2->print();
-        point2D_3->print();
+        point1->print();
+        point2->print();
+        point3->print();
     }
     
-    double sideA_2D() {
-        return sqrt(pow(point2D_2->getX() - point2D_1->getX(), 2) + pow(point2D_2->getY() - point2D_1->getY(), 2));
+
+    double sideA() {
+        return sqrt(pow(point2->getX() - point1->getX(), 2) + pow(point2->getY() - point1->getY(), 2) + pow(point2->getZ() - point1->getZ(), 2));
     }
 
-    double sideB_2D() {
-        return sqrt(pow(point2D_3->getX() - point2D_1->getX(), 2) + pow(point2D_3->getY() - point2D_1->getY(), 2));
+    double sideB() {
+        return sqrt(pow(point3->getX() - point1->getX(), 2) + pow(point3->getY() - point1->getY(), 2) + pow(point3->getZ() - point1->getZ(), 2));
     }
 
-    double sideC_2D() {
-        return sqrt(pow(point2D_3->getX() - point2D_2->getX(), 2) + pow(point2D_3->getY() - point2D_2->getY(), 2));
+    double sideC() {
+        return sqrt(pow(point3->getX() - point2->getX(), 2) + pow(point3->getY() - point2->getY(), 2) + pow(point3->getZ() - point2->getZ(), 2));
     }
 
-    double area_2D() {
-        double p = (sideA_2D() + sideB_2D() + sideC_2D()) / 2;
+    double area() {
+        double p = (sideA() + sideB() + sideC()) / 2;
 
-        return sqrt(p * (p - sideA_2D()) * (p - sideB_2D()) * (p - sideC_2D()));
-    }
-
-    double sideA_3D() {
-        return sqrt(pow(point3D_2->getX() - point3D_1->getX(), 2) + pow(point3D_2->getY() - point3D_1->getY(), 2) + pow(point3D_2->getZ() - point3D_1->getZ(), 2));
-    }
-
-    double sideB_3D() {
-        return sqrt(pow(point3D_3->getX() - point3D_1->getX(), 2) + pow(point3D_3->getY() - point3D_1->getY(), 2) + pow(point3D_3->getZ() - point3D_1->getZ(), 2));
-    }
-
-    double sideC_3D() {
-        return sqrt(pow(point3D_3->getX() - point3D_2->getX(), 2) + pow(point3D_3->getY() - point3D_2->getY(), 2) + pow(point3D_3->getZ() - point3D_2->getZ(), 2));
-    }
-
-    double area_3D() {
-        double p = (sideA_2D() + sideB_2D() + sideC_2D()) / 2;
-
-        return sqrt(p * (p - sideA_2D()) * (p - sideB_2D()) * (p - sideC_2D()));
-    }
-
-    double height() {
-        return ( 2 * area()) / 2;
+        return sqrt(p * (p - sideA()) * (p - sideB()) * (p - sideC()));
     }
 
 };
 
 class Tetragon : public FlatFigure {
 protected:
-    Point* point1;
-    Point* point2;
-    Point* point3;
-    Point* point4;
+    Section* side1;
+    Section* side2;
 
 public:
-    Tetragon(Point* point1, Point* point2, Point* point3, Point* point4) : point1(point1), point2(point2), point3(point3), point4(point4) {}
+    Tetragon(Section* side1, Section* side2) : side1(side1), side2(side2) {}
     void print() {
-        std::cout << "Tetragon ";
-        point1->print();
-        point2->print();
-        point3->print();
-        point4->print();
+        cout << "Tetragon ";
+        side1->print();
+        side1->print();
     }
 
     double area() {
@@ -186,13 +168,12 @@ public:
 int main() {
     setlocale(LC_ALL, "Rus");
 
-    Point2D a(1, 2);
-    Point2D b(3, 4);
-    Point2D c(1, 3);
+    Point3D a(-3, -8, -8);
+    Point3D b(4, -2, -1);
+    Point3D c(10, 8, 5);
 
     Triangle s(&a, &b, &c);
-
-    s.print();
+    cout << fixed << setprecision(6) << s.area();
 
     return 0;
 }
