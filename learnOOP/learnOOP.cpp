0cbb8e39f25c3d66558296bc00dadff7c0bc37d4
Kostess <qaz1234567890838@gmail.com>
2024-02-14 03:10:40 +0300

    модуль 6

--- learnOOP/learnOOP.cpp
+++ learnOOP/learnOOP.cpp
@@ -1,181 +1,33 @@
-﻿#include <iostream>
-#include <iomanip>
+#include <iostream>
+#include <set>
+#include <vector>
 
 using namespace std;
 
-class Figure {
-public:
-    void print() {}
-};
-
-class Point : public Figure {
-protected:
-    double _x{ 0.0 };
-
-public:
-    Point(double x) : _x(x) {};
-
-    ~Point() {
-        _x = 0.0;
-    }
-
-public:
-    void print() {
-        cout << "Point (" << _x << ")" << endl;
-    }
-
-    double getX() {
-        return this->_x;
-    }
-};
-
-class Point2D : public Point {
-protected:
-    double _y{ 0.0 };
-
-public:
-    Point2D(double x, double y) : Point(x), _y(y) {};
-
-    ~Point2D() {
-        _y = 0.0;
-    }
-
-public:
-    void print() {
-        cout << "Point2D (" << _x << ", " << _y << ")" << endl;
-    }
-
-    double getY() {
-        return this->_y;
-    }
-};
-
-class Point3D : public Point2D {
-protected:
-    double _z{ 0.0 };
-
-public:
-    Point3D(double x, double y) : Point2D(x, y) {};
-
-    Point3D(double x, double y, double z) : Point3D(x, y)  {
-        _z = z;
-    };
-
-    ~Point3D() {
-        _z = 0.0;
-    }
-
-public:
-    void print() {
-        cout << "Point3D (" << _x << ", " << _y << ", " << _z << ")" << endl;
-    }
-
-    double getZ() {
-        return this->_z;
-    }
-};
-
-class Section : public Figure {
-private:
-    Point3D* startPoint;
-    Point3D* endPoint;
-
-public:
-    Section(Point3D* startPoint, Point3D* endPoint) : startPoint(startPoint), endPoint(endPoint) {}
-
-    ~Section() {
-
-    }
-
-public:
-    void print() {
-        cout << "Section ";
-        startPoint->print();
-        endPoint->print();
-    }   
-};
-
-class FlatFigure : public Figure {
-public:
-    double area() {
-        return 0.0;
-    }
-};
-
-class Triangle : public FlatFigure {
-protected:
-    Point3D* point1;
-    Point3D* point2;
-    Point3D* point3;
-
-public:
-    Triangle(Point3D* point1, Point3D* point2, Point3D* point3) : point1(point1), point2(point2), point3(point3) {}
-
-    ~Triangle() {
-
-    }
-
-public:
-    void print() {
-        cout << "Triangle ";
-        point1->print();
-        point2->print();
-        point3->print();
-    }
-    
-
-    double sideA() {
-        return sqrt(pow(point2->getX() - point1->getX(), 2) + pow(point2->getY() - point1->getY(), 2) + pow(point2->getZ() - point1->getZ(), 2));
-    }
-
-    double sideB() {
-        return sqrt(pow(point3->getX() - point1->getX(), 2) + pow(point3->getY() - point1->getY(), 2) + pow(point3->getZ() - point1->getZ(), 2));
-    }
-
-    double sideC() {
-        return sqrt(pow(point3->getX() - point2->getX(), 2) + pow(point3->getY() - point2->getY(), 2) + pow(point3->getZ() - point2->getZ(), 2));
-    }
-
-    double area() {
-        double p = (sideA() + sideB() + sideC()) / 2;
-
-        return sqrt(p * (p - sideA()) * (p - sideB()) * (p - sideC()));
-    }
-
-};
-
-class Tetragon : public FlatFigure {
-protected:
-    Section* side1;
-    Section* side2;
-
-public:
-    Tetragon(Section* side1, Section* side2) : side1(side1), side2(side2) {}
-    void print() {
-        cout << "Tetragon ";
-        side1->print();
-        side1->print();
-    }
-
-    double area() {
-        // Реализация вычисления площади четырехугольника
-        double area = 0.0; // Замените на фактический расчет площади
-        return area;
-    }
-
-};
-
 int main() {
-    setlocale(LC_ALL, "Rus");
+    int n, m;
+    cin >> n >> m;
 
-    Point3D a(-3, -8, -8);
-    Point3D b(4, -2, -1);
-    Point3D c(10, 8, 5);
+    multiset<int> set;
 
-    Triangle s(&a, &b, &c);
-    cout << fixed << setprecision(6) << s.area();
+    for (int i = 0; i < n; i++) {
+        int num;
+        cin >> num;
+        set.insert(num);
+    }
+
+    for (int i = 0; i < m; i++) {
+        set.insert(80);
+    }
+
+    int min = *set.begin();
+    for (auto it = set.begin(); it != set.end(); ++it) {
+        if (*it < min) {
+            min = *it;
+        }
+    }
+
+    cout << min;
 
     return 0;
 }
-
-
--- task/task.cpp
+++ task/task.cpp
@@ -1,131 +1,47 @@
 ﻿#include <iostream>
-#include <cmath>
+#include <set>
+#include <algorithm>
 
-class Figure {
-public:
-    virtual void print() = 0;
-};
+using namespace std;
 
-class Point : public Figure {
-protected:
-    int x;
-public:
-    Point(int x) : x(x) {}
+set<int> symmetric_difference(set<int> set1, set<int> set2) {
+    set<int> resultSet;
 
-    void print() override {
-        std::cout << "Point (" << x << ")" << std::endl;
-    }
-    int getX() {
-        return x;
-    }
-};
+    set_difference(set1.begin(), set1.end(),
+        set2.begin(), set2.end(),
+        inserter(resultSet, resultSet.begin()));
 
-class Point2D : public Point {
-protected:
-    int y;
-public:
-    Point2D(int x, int y) : Point(x), y(y) {}
-    void print() override {
-        std::cout << "Point2D (" << x << ", " << y << ")" << std::endl;
-    }
-    int getY() {
-        return y;
-    }
-};
+    set_difference(set2.begin(), set2.end(),
+        set1.begin(), set1.end(),
+        inserter(resultSet, resultSet.begin()));
 
-class Point3D : public Point2D {
-protected:
-    int z;
-public:
-    Point3D(int x, int y, int z) : Point2D(x, y), z(z) {}
-    void print() override {
-        std::cout << "Point3D (" << x << ", " << y << ", " << z << ")" << std::endl;
-    }
-    int getZ() {
-        return z;
-    }
-};
-
-class Section : public Figure {
-protected:
-    Point* startPoint;
-    Point* endPoint;
-public:
-    Section(Point* startPoint, Point* endPoint) : startPoint(startPoint), endPoint(endPoint) {}
-    void print() override {
-        std::cout << "Section ";
-        startPoint->print();
-        endPoint->print();
-    }
-};
-
-class FlatFigure : public Figure {
-public:
-    virtual float getArea() = 0;
-};
-
-class Triangle : public FlatFigure {
-protected:
-    Point3D* point1;
-    Point3D* point2;
-    Point3D* point3;
-public:
-    Triangle(Point3D* point1, Point3D* point2, Point3D* point3) : point1(point1), point2(point2), point3(point3) {}
-    void print() override {
-        std::cout << "Triangle ";
-        point1->print();
-        point2->print();
-        point3->print();
-    }
-    float getArea() override {
-        // Реализация вычисления площади треугольника
-        float a = calculateDistance(point2, point3);
-        float b = calculateDistance(point1, point3);
-        float c = calculateDistance(point1, point2);
-        float p = (a + b + c) / 2;
-        float area = sqrt(p * (p - a) * (p - b) * (p - c));
-        return area;
-    }
-    float calculateDistance(Point3D* point1, Point3D* point2) {
-        float distance = sqrt(pow(point2->getX() - point1->getX(), 2) + pow(point2->getY() - point1->getY(), 2) + pow(point2->getZ() - point1->getZ(), 2));
-        return distance;
-    }
-};
-
-class Tetragon : public FlatFigure {
-protected:
-    Point* point1;
-    Point* point2;
-    Point* point3;
-    Point* point4;
-public:
-    Tetragon(Point* point1, Point* point2, Point* point3, Point* point4) : point1(point1), point2(point2), point3(point3), point4(point4) {}
-    void print() override {
-        std::cout << "Tetragon ";
-        point1->print();
-        point2->print();
-        point3->print();
-        point4->print();
-    }
-    float getArea() override {
-        // Реализация вычисления площади четырехугольника
-        float area = 0.0; // Замените на фактический расчет площади
-        return area;
-    }
-};
+    return resultSet;
+}
 
 int main() {
-    Point p1(1);
-    Point2D p2(2, 3);
-    Point3D p3(4, 5, 6);
-    Section s1(&p1, &p2);
-    Triangle t1(&p3);
-    Tetragon t2(&p1, &p2, &p3, &p3); // Обратите внимание: point4 должен быть присвоен &p4, а не &p3
-    p1.print();
-    p2.print();
-    p3.print();
-    s1.print();
-    t1.print();
-    t2.print();
+    setlocale(LC_ALL, "ru");
+
+    set<int> set1{ 0, 1, 2, 4 };
+    set<int> set2{ 1, 2, 3, 5 };
+    set<int> resultSet = symmetric_difference(set1, set2);
+
+    cout << "set1: ";
+    for (auto i = set1.cbegin(); i != set1.cend(); ++i) {
+        cout << *i << " ";
+    }
+    cout << endl;
+
+    cout << "set2: ";
+    for (auto i = set2.cbegin(); i != set2.cend(); ++i) {
+        cout << *i << " ";
+    }
+    cout << endl;
+
+    cout << "resultSet: ";
+    for (auto i = resultSet.cbegin(); i != resultSet.cend(); ++i) {
+        cout << *i << " ";
+    }
+    cout << endl;
+
     return 0;
 }
